8e7a647051dc797feac0341fd4c82ad11444cad5
Oc-found <153354935+Oc-found@users.noreply.github.com>
2024-03-22 19:53:05 +0800

    Delete OcC directory

--- OcC/fuz.py
+++
@@ -1,7 +0,0 @@
-from num import num
-
-def fUz(z):
-    c = z.find('=')
-    if c != -1:
-        return [z[0:c],z[c+1:]]
-    #return num(z)
--- OcC/run.py
+++
@@ -1,6 +0,0 @@
-from cha import cha
-cs = {}
-usr = ''
-while usr != 'exit':
-    usr = input('--@ ')
-    print(cha(usr))
